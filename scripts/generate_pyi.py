89da238414bc1b6ed97d04f147a6fe52da4f710c
pgmesa <pgmesa.sm@gmail.com>
2025-06-15 01:31:12 +0200

    Improve stub generation

--- scripts/generate_pyi.py
+++ scripts/generate_pyi.py
@@ -12,6 +12,31 @@ if project_path not in sys.path:
 def main():
     command = ['pybind11-stubgen', 'synapx._C', '--output', '.']
     subprocess.run(command, cwd=project_path)
+    
+    stub_path = project_path / 'synapx/_C.pyi'
+    if stub_path.exists():
+        with open(stub_path, 'r') as file:
+            content = file.read()
+            
+            # Add torch import
+            content = content.replace("import numpy", "import torch\nimport numpy")
+            
+            # Replace grad return value 
+            content = content.replace(
+                "def grad(self) -> typing.Any:", 
+                "def grad(self) -> typing.Union[Tensor, None]:"
+            )
+            
+            # Wrong generated type
+            content = content.replace("numpy.dtype[typing.Any]", "torch.dtype")
+            
+            # Improve device annotation
+            content = content.replace(": device", ": torch.device")
+            
+        with open(stub_path, 'w') as file:
+            file.write(content)
+    else:
+        print("[!] Stub file does not exist")
 
 
 if __name__ == "__main__":
