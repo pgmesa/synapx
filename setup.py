1360eaa788e163f3d23381b565cb33f5b41ba930
pgmesa <pgmesa.sm@gmail.com>
2025-07-07 03:49:32 +0200

    Only add stub.c source file when building on windows

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from pathlib import Path
 from setuptools import setup, Extension
 
@@ -21,20 +22,24 @@ PyMODINIT_FUNC PyInit__C(void)
 }
 """
 
-# Minimal stub
-stub_file = cwd / 'stub.c'
-with open(stub_file, 'w') as f:
-    f.write(stub_c)
-
 synapx_lib_dir = cwd / 'synapx/lib'
 lib_dirs = [str((synapx_lib_dir/d).relative_to(cwd)) for d in os.listdir(synapx_lib_dir)]
+
+main_sources = []
+if platform.system() == 'Windows':
+    # Minimal stub
+    stub_file = cwd / 'stub.c'
+    with open(stub_file, 'w') as f:
+        f.write(stub_c)
+    
+    main_sources.append('stub.c')
 
 extensions = [
     Extension(
         'synapx._C',
         libraries=['synapx'],
         language="C++",
-        sources=['stub.c'],
+        sources=main_sources,
         library_dirs=lib_dirs
     )
 ]
