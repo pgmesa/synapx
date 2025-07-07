3d6024eb406fec72cde0dfc8331956802ab8fa00
pgmesa <pgmesa.sm@gmail.com>
2025-07-07 03:40:24 +0200

    Add a stub.c autogenerated file to the _C extension source files

--- setup.py
+++ setup.py
@@ -1,46 +1,44 @@
-
 import os
-import platform
 from pathlib import Path
 from setuptools import setup, Extension
-from wheel.bdist_wheel import bdist_wheel
 
 
-system = platform.system()
 cwd = Path(__file__).parent.absolute()
 
+stub_c = """
+#include <Python.h>
 
-if system == 'Linux':
-    synapx_lib_dir = cwd / 'synapx/lib'
-    lib_dirs = [str((synapx_lib_dir/d).relative_to(cwd)) for d in  os.listdir(synapx_lib_dir)]
-    print(lib_dirs)
+#ifndef _WIN32
+#ifdef __cplusplus
+extern "C"
+#endif
+__attribute__((visibility("default"))) PyObject* PyInit__C(void);
+#endif
 
-    extensions = [
-        Extension(
-            'synapx._C',
-            libraries=['synapx'],
-            language="C++",
-            sources=[],
-            library_dirs=lib_dirs
-        )
-    ]
+PyMODINIT_FUNC PyInit__C(void)
+{
+  return NULL;
+}
+"""
 
-    setup(
-        ext_modules=extensions,
+# Minimal stub
+stub_file = cwd / 'stub.c'
+with open(stub_file, 'w') as f:
+    f.write(stub_c)
+
+synapx_lib_dir = cwd / 'synapx/lib'
+lib_dirs = [str((synapx_lib_dir/d).relative_to(cwd)) for d in os.listdir(synapx_lib_dir)]
+
+extensions = [
+    Extension(
+        'synapx._C',
+        libraries=['synapx'],
+        language="C++",
+        sources=['stub.c'],
+        library_dirs=lib_dirs
     )
-    
-elif system == 'Windows':
-    class BDistWheelCustom(bdist_wheel):
-        
-        def finalize_options(self):
-            bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
+]
 
-    setup(
-        cmdclass={
-            'bdist_wheel': BDistWheelCustom,
-        },
-    )
-    
-else:
-    raise RuntimeError(f"Current system is not supported '{system}'")
+setup(
+    ext_modules=extensions,
+)
